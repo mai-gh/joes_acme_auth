ab1b579a0835b749fdc0745552aab63eeeca93c1
Mai <jea989@gmail.com>
2022-02-15 17:08:07 -0500

    can display notes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const { models: { User } } = require('./db');
+const { models: { User, Note } } = require('./db');
 const path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
@@ -10,6 +10,20 @@ app.post('/api/auth', async (req, res, next) => {
     // req.body = { username: "lucy", password: "lucy_pw" }
     try {
         res.send({ token: await User.authenticate(req.body) });
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.get('/api/users/:id/notes', async (req, res, next) => {
+    try {
+       console.log('aaaaaaaaaaa')
+//        res.send(await User.byToken(req.headers.authorization));
+       const notes = await Note.findAll({where: {userId: req.params.id}})
+       //const notes = await User.findAll({where: {id: req.params.id}, include: {model: Note}})
+       
+       res.send(notes)
     }
     catch (ex) {
         next(ex);
--- db.js
+++ db.js
@@ -16,6 +16,13 @@ const User = conn.define('user', {
     password: STRING
 });
 
+const Note = conn.define('note', {
+    text: STRING,
+});
+
+User.hasMany(Note);
+Note.belongsTo(User);
+
 
 
 User.byToken = async (token) => {
@@ -73,6 +80,12 @@ const syncAndSeed = async () => {
     const [lucy, moe, larry] = await Promise.all(
         credentials.map(credential => User.create(credential))
     );
+
+    const notes = [ { text: 'hello world'}, { text: 'reminder to buy groceries'}, { text: 'reminder to do laundry'} ];
+    const [note1, note2, note3] = await Promise.all(notes.map( note => Note.create(note)));
+    await lucy.setNotes(note1);
+    await moe.setNotes([note2, note3]);
+
     return {
         users: {
             lucy,
@@ -85,6 +98,7 @@ const syncAndSeed = async () => {
 module.exports = {
     syncAndSeed,
     models: {
-        User
+        User,
+        Note
     }
 };
